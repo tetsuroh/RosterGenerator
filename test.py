cbc5dc9f8cad9817d8d4ac6a24b1c0a2fc6a3031
Tetsuroh <tetsuroh.js@gmail.com>
2012-12-13 15:50:27 +0900

    tetetest

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 import unittest
+
 from rg import util
+from rg import *
+
 class test_util(unittest.TestCase):
     def test_flip(self):
         res = []
         for i in range(1000):
-            res.append(util.flip(0.5))
-        len(list(filter((lambda x: x), res)))
+            res.append(util.flip.flip(0.5))
+        l = len(list(filter((lambda x: x), res)))
+        self.assertTrue(530 > l > 470)
+
+class test_work(unittest.TestCase):
+    def setUp(self):
+        self.w = Work()
+
+    def test(self):
+        self.assertEqual(self.w.work, '')
+        self.assertEqual(self.w.locked, False)
+        self.w.work = 'A'
+        self.w.locked = True
+        self.assertEqual(self.w.work, 'A')
+        self.assertEqual(self.w.locked, True)
+
+if __name__ == '__main__':
+    unittest.main()
