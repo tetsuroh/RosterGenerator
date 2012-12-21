1dac3a46710cdf0ef060d915805a47be4d61d2ea
Tetsuroh <tetsuroh.js@gmail.com>
2012-12-21 15:28:42 +0900

    Fix rg.util.radom_roster and rg.__init__.py

--- rg/__init__.py
+++ rg/__init__.py
@@ -2,6 +2,9 @@ from .roster.work import Work
 from .roster.shift import Shift
 from .roster.roster import Roster
 from .roster.employee import Employee
+from .util.roster2csv import convert
+from .util.random_roster import randomize, rand
+from .util.flip import flip
 
 __major__ = 0
 __minor__ = 0
@@ -9,4 +12,5 @@ __release__ = 0
 
 __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
-__all__ = ["Work", "Shift", "Roster", "Employee"]
+__all__ = ["Work", "Shift", "Roster", "Employee",
+           "convert", "randomize", "rand", "flip"]
--- rg/util/random_roster.py
+++ rg/util/random_roster.py
@@ -5,9 +5,9 @@ def rand(max, min=0):
 
 def randomize(roster):
     for shift in roster:
-        employee = shift.employee()
+        employee = shift.employee
         for work in shift:
-            if not work.locked():
-                l = len(employee.works())
-                work.set_work(employee.works()[rand(l)])
+            if not work.locked:
+                l = len(employee.works)
+                work.work = employee.works[rand(l)]
     return roster
