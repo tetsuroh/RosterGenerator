3bac57859b11f5c4ba747eb321434217d3aa0f96
Tetsuroh <tetsuroh.js@gmail.com>
2012-12-25 17:03:38 +0900

    GA.py ...It...It works!!!

--- rg/GA.py
+++ rg/GA.py
@@ -1,5 +1,6 @@
-import random
 from .util.flip import flip
+from .util.random_roster import rand
+
 
 class Entity:
     def __init__(self,
@@ -7,9 +8,9 @@ class Entity:
                  mRate,
                  mParam
                  ):
-        self.gene                = gene
-        self.mutaion_rate        = mRate
-        self.mutaion_parameter   = mParam
+        self.gene = gene
+        self.mutaion_rate = mRate
+        self.mutaion_parameter = mParam
 
         self._fitness = None
 
@@ -30,7 +31,7 @@ class Entity:
 
     def __ge__(self, o):
         return self.fitness >= o.fitness
-    
+
     def isPerfect(self):
         """
         This method is virtual function
@@ -38,7 +39,7 @@ class Entity:
 
         This method examines whether or not this entity is perfect.
         """
-        pass
+        return self.fitness == 0
 
     def mutation(self):
         """
@@ -47,7 +48,12 @@ class Entity:
 
         This method is to mutate the entity
         """
-        pass
+        if not flip(self.mutaion_rate):
+            return
+
+        for i in range(len(self.gene)):
+            if flip(self.mutaion_parameter):
+                self.gene[i] = 0 if self.gene[i] else 1
 
     @property
     def fitness(self):
@@ -61,19 +67,17 @@ class Entity:
     def fitness(self):
         del self._fitness
 
-def rand(max, min=0):
-    return int(random.random() * (max - min) + min)
 
 class GA:
     def __init__(self,
-                 popSize=100,  # population size
-                 aSize=10,     # archive size
-                 maxGene=100 , # maximum number of generations
+                 popSize=800,  # population size
+                 aSize=30,     # archive size
+                 maxGene=50,  # maximum number of generations
                  cRate=0.8,    # crossover rate
                  mRate=0.06,   # mutation rate
                  cParam=0.5,   # parameter for crossover
                  mParam=0.02,  # parameter for mutation
-                 tSize=20      # tournament size
+                 tSize=15      # tournament size
                  ):
         self.population_size = popSize
         self.archive_size = aSize
@@ -83,28 +87,33 @@ class GA:
         self.crossover_parameter = cParam
         self.mutaion_parameter = mParam
         self.tournament_size = tSize
-        
-        self.initialize_population()
+
         self.entities = []
+        self.initialize_population()
         self.next_generation = []
         self.generation = 0
-        self.log = []
+        self.log = ""
 
     def initialize_population(self):
         """
         This method is virtual function.
         Please override in subclass.
 
-        
         """
-        for i in range(self.population_size):
+        GENOM_LEN = 400
+
+        def random_gene(length):
             gene = []
-            for j in range(30):
+            for j in range(length):
                 b = 1 if flip(0.5) else 0
                 gene.append(b)
-            self.entities[i] = Entity(gene,
-                                      self.mutaion_rate,
-                                      self.mutaion_parameter)
+            return gene
+        for i in range(self.population_size):
+            gene = random_gene(GENOM_LEN)
+            self.entities.append(Entity(gene,
+                                        self.mutaion_rate,
+                                        self.mutaion_parameter))
+        self.answer = random_gene(GENOM_LEN)
 
     def tournament_selection(self):
         '''
@@ -134,44 +143,77 @@ class GA:
         if not flip(self.crossover_rate):
             return ()
         else:
-            for i in range(mother.gene):
+            for (m, f) in zip(mother.gene, father.gene):
                 if flip(self.crossover_parameter):
-                    child1.gene.append(mother.gene[i])
-                    child2.gene.append(father.gene[i])
+                    child1.gene.append(m)
+                    child2.gene.append(f)
                 else:
-                    child1.gene.append(father.gene[i])
-                    child2.gene.append(mother.gene[i])
+                    child1.gene.append(f)
+                    child2.gene.append(m)
             return (child1, child2)
-    
+
     def perform_archive(self):
         self.next_generation = self.entities[:self.archive_size]
 
     def perform_crossover(self):
-        for _ in range((self.population_size - self.archive_size) / 2):
+        for _ in range(int((self.population_size - self.archive_size) / 2)):
             children = None
             while not children:
-                (c1, c2) = self.crossover(self.tournament_selection())
-            self.next_generation.append(c1)
-            self.next_generation.append(c2)
+                children = self.crossover(*self.tournament_selection())
+            self.next_generation.append(children[0])
+            self.next_generation.append(children[1])
+
+    def perform_mutation(self):
+        if not flip(self.mutaion_rate):
+            return
+        for entity in self.entities:
+            entity.mutation()
 
     def calc_fitness(self):
-        pass
+        """
+        This method calculates fitness for every entities.
+        This method is virtual function.
+        Please override in subclass.
+        """
+        for e in self.entities:
+            fitness = 0
+            for (a, b) in zip(self.answer, e.gene):
+                if a != b:
+                    fitness += 1
+            e.fitness = fitness
+
+    def sort_entities(self):
+        def sort(es):
+            if not es:
+                return []
+            else:
+                head = es.pop(0)
+                return sort([e for e in es if e < head]) + [head] +\
+                    sort([e for e in es if e >= head])
+        self.entities = sort(self.entities)
 
     def evolve(self):
         self.next_generation = []
         self.generation += 1
-        self.calc_fitness()
-        self.sort_entities()
+
         self.perform_archive()
         self.perform_crossover()
         # Alternations to the next generation.
         self.entities = self.next_generation
-        self.perform_mutate()
+        self.perform_mutation()
 
     def evolve_verbose(self):
         while self.generation < self.max_generations:
+            self.calc_fitness()
+            self.sort_entities()
+            print("""
+Generation: %d
+Fitness: %d""" % (self.generation, self.entities[0].fitness))
             if (self.entities[0].isPerfect()):
                 return self.entities[0]
             else:
                 self.evolve()
-                self.log = ""
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
