b5bd6a0589f86eb667638a5c8c151ad420c974a4
Tetsuroh <tetsuroh.js@gmail.com>
2012-12-30 22:31:24 +0900

    Adding settings.py. module for read and write settings.

---
+++ rg/util/settings.py
@@ -0,0 +1,13 @@
+import json
+
+__all__ = ["load", "save"]
+
+
+def load(filename, encoding="utf-8"):
+    with open(filename, "r", encoding=encoding) as fp:
+        return json.load(fp)
+
+
+def save(obj, filenmae, encoding="utf-8"):
+    with open(filename, "w", encoding=encoding) as fp:
+        fp.write(json.dumps(obj, indent=2, ensure_ascii=False))
