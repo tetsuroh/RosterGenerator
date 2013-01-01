f664eb66088658528f61c75a61e67fbf07a833d8
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-01 13:52:50 +0900

    Adding test for rg_app.REntity.mutation

--- test.py
+++ test.py
@@ -5,6 +5,18 @@ from rg import *
 import rg_app
 
 
+def foldl1(fn, ls):
+    def foldl(fn, x, ls):
+        if not ls:
+            return x
+        else:
+            return foldl(fn, fn(x, ls.pop()), ls)
+    if len(ls) == 1:
+        return ls[0]
+    else:
+        return foldl(fn, ls.pop(), ls)
+
+
 class TestRGApp(unittest.TestCase):
     def setUp(self):
         self.rgapp = rg_app.RGApp("./settings/sunhome_kitchen.json")
@@ -23,6 +35,33 @@ class TestRGApp(unittest.TestCase):
         self.assertFalse(e1 == e2)
         self.assertTrue(e1 <= e2)
         self.assertTrue(e1 != e2)
+
+    def test_rentity_mutation(self):
+        """
+        test_rentity_mutation
+        Arguments:
+        - `self`:
+        """
+        e = self.rgapp.entities[0]
+        r = []
+        for shift in e.gene:
+            s = []
+            for day in shift:
+                s.append(day.work)
+            r.append(s)
+        e.mutation_rate = 1
+        e.mutation_parameter = 0.5
+        e.mutation()
+        same = 0
+        diff = 0
+        for (es, rs) in zip(e.gene, r):
+            for (ed, rd) in zip(es, rs):
+                if ed.work == rd:
+                    same += 1
+                else:
+                    diff += 1
+        print("Difference is %d." % (same - diff))
+        self.assertTrue(diff != 0)
 
 
 class TestGa(unittest.TestCase):
