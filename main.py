9ca5ca5ec942f213d31187e1b738b00a320cf7fe
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-04 20:14:58 +0900

    import RGApp

--- main.py
+++ main.py
@@ -1,72 +1,20 @@
-from rg import *
-import json
+from rg import convert
+from rg_app import RGApp
+
+from time import time
 
 
 def main():
-    # load settings
-    settings = load_setting("./settings/", "sunhome_kitchen.json")
-
-    # lastday of the month
-    lastday_of_the_month = 31
-
-    # initialize employees
-    employees = [
-        Employee(name, status) for (name, status)in [
-            ("サイトウ", "常勤"),
-            ("モリ", "常勤"),
-            ("マスジマ", "常勤"),
-            ("ソンダ", "常勤"),
-            ("ヤマモト", "パート"),
-            ("サトウ", "パート"),
-            ("コンノ", "パート")]]
-
-    # 従業員のシフト内容を設定
-    for employee in employees:
-        employee.works = settings['works'][employee.status]
-
-    rosters = []
-    for i in range(800):
-        rosters.append(Roster(lastday_of_the_month, employees))
-
-    csv = convert(randomize(rosters[0]))
+    """
+    Generate roster using Genetic Algorithm.
+    """
+    start = time()
+    rgapp = RGApp("./settings/sunhome_kitchen.json")
+    roster = rgapp.evolve_verbose().gene
     with open("out.csv", mode="w", encoding="utf-8") as filep:
-        filep.write(csv)
-    print("complete")
-    input()
-    '''
-    rostersに対してランダムにあれこれしたりしてほげほげする。
-    Date関連と乱数関連のことを調べての頃のとこを実装スべし。
-    '''
-
-
-def sort_rosters(rosters):
-    if not rosters:
-        return []
-    else:
-        head = rosters.pop(0)
-        head_problems = check(head)
-        return [r for r in rosters if check(r) <= head_problems] + \
-            [head] + \
-            [r for r in rosters if check(r) > head_problems]
-
-
-class RosterChecker:
-    def __init__(self):
-        pass
-
-    def check(self, roster):
-        problems = 0
-        problems += self.check_day(roster)
-        problems += self.check_holiday(roster)
-        return problems
-
-    def check_day_on():
-        pass
-
-
-def check(roster):
-    problem = 0
-    return problem
+        filep.write(convert(roster))
+    end = time()
+    print("complete in %d seconds." % (end - start))
 
 if __name__ == '__main__':
     main()
