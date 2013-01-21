e78864ca49b7db089eae64f43f4f94439686b654
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-21 16:29:36 +0900

    I change it to take arguments year, month, day and weekday.

--- rg/roster/work.py
+++ rg/roster/work.py
@@ -1,24 +1,47 @@
 
 
 class Work:
-    def __init__(self, work="", locked=False):
+    def __init__(self,
+                 work="",
+                 locked=False,
+                 year=1970,
+                 month=1,
+                 day=1,
+                 weekday=3):
         '''
         >>> work = Work('日勤', True)
-        >>> work.work()
+        >>> work.work
         '日勤'
-        >>> work.set_work('早番').work()
+        >>> work.set_work('早番').work
         '早番'
-        >>> work.locked()
+        >>> work.locked
         True
-        >>> work.set_locked(False).locked()
+        >>> work.set_locked(False).locked
         False
         '''
-        self.work = work
+        self._work = work
         self.locked = locked
+        self.year = year
+        self.month = month
+        self.day = day
+        self.weekday = weekday
+
+    @property
+    def work(self):
+        return self._work
+
+    @work.setter
+    def work(self, work):
+        if not self.locked:
+            self._work = work
+        else:
+            raise PermissionError("""
+        You can't set value to locked work object.
+        """)
 
     def __str__(self):
         locked = " Locked " if self.locked else "Unlocked"
-        return "(%s, %s)" % (self.work, locked)
+        return "(%s, %s)" % (self._work, locked)
 
     def __repr__(self):
         return str(self)
