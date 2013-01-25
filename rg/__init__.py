68962a494843c68395261bf1642aef24215c7f4c
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-25 22:12:01 +0900

    Adding rg/util/fold.py fold a list like hakell.

--- rg/__init__.py
+++ rg/__init__.py
@@ -1,11 +1,12 @@
-from .roster.work import Work
-from .roster.shift import Shift
-from .roster.roster import Roster
-from .roster.employee import Employee
-from .util.roster2csv import convert
-from .util.random_roster import randomize, rand
-from .util.flip import flip
-from .GA import Entity, GA
+from rg.roster.work import Work
+from rg.roster.shift import Shift
+from rg.roster.roster import Roster
+from rg.roster.employee import Employee
+from rg.util.roster2csv import convert
+from rg.util.random_roster import randomize, rand
+from rg.util.flip import flip
+from rg.util.fold import fold
+from rg.GA import Entity, GA
 
 __major__ = 0
 __minor__ = 0
@@ -15,4 +16,4 @@ __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
 __all__ = ["Work", "Shift", "Roster", "Employee",
            "convert", "randomize", "rand", "flip",
-           "GA", "Entity"]
+           "fold", "GA", "Entity"]
