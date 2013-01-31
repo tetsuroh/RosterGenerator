026609f30e5d5b79a9724d549613fcfe2d97cfac
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-31 22:14:50 +0900

    Adding inter face to main.py.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from rg import convert
+from rg.util import csv
 from rg.app import RGApp
 
 from time import time
+import os
+import re
 
 
 def main():
@@ -9,15 +11,27 @@ def main():
     Generate roster using Genetic Algorithm.
     """
     start = time()
-    rgapp = RGApp("./settings/sunhome_kitchen.json")
+    setting_files = list(filter(lambda x: x.endswith('.json'),
+                                os.listdir('settings')))
+    while True:
+        for i, f in enumerate(setting_files):
+            print("%d, %s" % (i + 1, f))
+        num = input("Please input selected file number >> ")
+        if re.match('^\d+$', num):
+                break
+    rgapp = RGApp("./settings/%s" %
+                  setting_files[int(num) - 1])
+    drop = len(rgapp.settings['last_month_data'])
     try:
-        rgapp.evolve_verbose().gene
+        rgapp.evolve_verbose()
     except KeyboardInterrupt:
         pass
     finally:
         roster = rgapp.best_entity.gene
+        print("Shift length is %d" % len(roster[0]))
         with open("out.csv", mode="w", encoding="utf-8") as filep:
-            filep.write(convert(roster, status=True))
+            filep.write(csv.from_roster(roster,
+                                        drop=drop))
     end = time()
     print("complete in %d seconds." % (end - start))
 
