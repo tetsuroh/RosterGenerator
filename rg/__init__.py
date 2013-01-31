5437271827d7d3ece77d02076d048b2252388de7
Tetsuroh <tetsuroh.js@gmail.com>
2013-01-31 22:16:32 +0900

    util.roster2csv -> util.csv

--- rg/__init__.py
+++ rg/__init__.py
@@ -2,7 +2,7 @@ from rg.roster.work import Work
 from rg.roster.shift import Shift
 from rg.roster.roster import Roster
 from rg.roster.employee import Employee
-from rg.util.roster2csv import convert
+from rg.util.csv import from_roster
 from rg.util.random_roster import randomize, rand
 from rg.util.flip import flip
 from rg.util.fold import fold
@@ -15,5 +15,5 @@ __release__ = 0
 __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
 __all__ = ["Work", "Shift", "Roster", "Employee",
-           "convert", "randomize", "rand", "flip",
+           "from_roster", "randomize", "rand", "flip",
            "fold", "GA", "Entity"]
